1978afe194c555dd21cba29da5c3dd49a97fb05a
Chase Vaughan <git@cvaugh.dev>
2022-11-18 00:02:00 -0500

    Add basic RetinaFace wrapper

--- fd_wrapper/retinaface.py
+++ fd_wrapper/retinaface.py
@@ -1,5 +1,12 @@
-import retinaface
+from retinaface import RetinaFace
+from tqdm import tqdm
 
-def classify(image):
-    # to do
-    print("RetinaFace unimplemented")
+def classify(paths):
+    if not isinstance(paths, list): paths = [paths]
+    results = []
+    progress = tqdm(paths)
+    progress.set_description("Classifying images")
+    for path in progress:
+        result = RetinaFace.detect_faces(path)
+        results.append(isinstance(result, dict))
+    return results
