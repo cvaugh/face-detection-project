02137980717dea88901b3df42bd34bbae2925d99
Chase Vaughan <git@cvaugh.dev>
2022-12-18 17:59:34 -0500

    Move graph_results into fd_wrapper module

--- fd_wrapper/__init__.py
+++ fd_wrapper/__init__.py
@@ -1,10 +1,13 @@
 import csv
 import os
+from colorsys import hsv_to_rgb
 from datetime import timedelta
 from fnmatch import filter
 from pathlib import Path
+from sys import stderr
 from time import time
 
+import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 from tqdm import tqdm
@@ -154,3 +157,99 @@ def write_results(paths, results, detectors, path, known=False, truth_override=N
             if known:
                 row.append(int("positive" in paths[i]) if truth_override is None else truth_override)
             writer.writerow(row)
+
+def graph_results(results_dir, detectors, results_file_filter="*.csv", out_file=None, title="Accuracy when classifying {image_count} labeled images",
+                  subtitle="", graphing=None, size=(1920, 1200), dpi=96, silent=False):
+    assert len(detectors) > 0, "Must pass at least one detector"
+    dets = [detector.name() for detector in detectors]
+    paths = []
+    for root, dirs, files in os.walk(results_dir):
+        for path in filter(files, results_file_filter):
+            paths.append(os.path.join(root, path))
+
+    if len(paths) == 0:
+        print("No results found", file=stderr)
+        return
+
+    shifts = [int(os.path.splitext(os.path.basename(path))[0]) for path in paths]
+
+    paths_sorted = np.array(paths)[np.argsort(shifts)[::1]]
+
+    shifts = np.sort(shifts)
+
+    entries = [None] * len(paths_sorted)
+
+    progress = tqdm(range(len(entries)), disable=silent)
+    progress.set_description("Loading results")
+    for i in progress:
+        entry = {"shift": shifts[i], "results": []}
+        with open(paths_sorted[i]) as f:
+            next(f) # skip header
+            r = csv.reader(f, delimiter=",")
+            for line in r:
+                e = np.array(line[1:len(line)], dtype=int)
+                entry["results"].append(e)
+        entry["results"] = np.array(entry["results"])
+        entries[i] = entry
+    progress.close()
+
+    results = [None] * len(dets)
+
+    image_count = len(entries[0]["results"])
+
+    fig, ax = plt.subplots(len(dets) + 1, sharex=True)
+    fig.suptitle(title.format(image_count=image_count))
+    fig.tight_layout()
+
+    graph_xlabel = subtitle
+
+    line_color = "blue" if graphing is None else "gray"
+
+    y_min = float('inf')
+    y_max = float('-inf')
+
+    for i in range(len(dets)):
+        results[i] = np.array([100 * np.sum(entry["results"][:, i] == entry["results"][:, len(dets)]) / image_count for entry in entries])
+        if np.any(results[i] != 0.0):
+            y_min = np.min([y_min, np.min(results[i])])
+            y_max = np.max([y_max, np.max(results[i])])
+
+    if y_min == y_max:
+        y_min -= 0.01
+        y_max += 0.01
+    y_range = y_max - y_min
+    y_padding = y_range / 10
+
+    progress = tqdm(range(len(dets)), disable=silent)
+    progress.set_description("Plotting results")
+    for i in progress:
+        ax[i].set_ylim(y_min - y_padding, y_max + y_padding)
+        ax[i].set_title(dets[i])
+        ax[i].set_ylabel("Accuracy (percent)")
+        ax[i].plot(shifts, results[i], color=line_color, linewidth=1)
+        if graphing is not None:
+            for j in range(0, np.max(shifts) + 1):
+                match graphing:
+                    case "hue":
+                        color = hsv_to_rgb(j / 255, 1, 1)
+                    case "saturation":
+                        color = hsv_to_rgb(0, j / 255, 0.9)
+                    case "value":
+                        color = hsv_to_rgb(0, 1, j / 255)
+                    case _:
+                        color = "red"
+                ax[i].plot(j, results[i][j], color=color, marker="o", markersize=3)
+        ax[len(dets)].plot(shifts, results[i], linewidth=1)
+    progress.close()
+        
+    ax[len(dets)].set_ylim(y_min - y_padding, y_max + y_padding)
+    ax[len(dets)].set_title("All detectors")
+    ax[len(dets)].set_ylabel("Accuracy (percent)")
+    ax[len(dets)].set_xlabel(graph_xlabel)
+    ax[len(dets)].legend(dets, loc="upper right", prop={"size": 8})
+
+    if out_file is None:
+        plt.show()
+    else:
+        plt.gcf().set_size_inches(size[0]/dpi, size[1]/dpi)
+        plt.savefig(out_file)
--- graph_results.py
+++ graph_results.py
@@ -1,113 +1,12 @@
-import os
-from colorsys import hsv_to_rgb
-from csv import reader
-from fnmatch import filter
-
-import matplotlib.pyplot as plt
-import numpy as np
-from tqdm import tqdm
-
 import fd_wrapper as wrapper
+from fd_wrapper.detectors import *
 
-results_dir = wrapper.relative_path("./results/200_old/hue", root=__file__)
-out_file = "hue.png"
-subtitle = "Hue (0-255)"
-graphing = "hue" # must be one of "hue", "saturation", "value", or None
-positive_only = True
-negative_only = False
-
-if positive_only and negative_only:
-    print("Only one of 'positive_only' and 'negative_only' may be true")
-    exit()
-
-paths = []
-for root, dirs, files in os.walk(results_dir):
-    for path in filter(files, "*.csv"):
-        paths.append(os.path.join(root, path))
-
-if len(paths) == 0:
-    print("No CSV files found")
-    exit()
-
-shifts = [int(os.path.splitext(os.path.basename(path))[0]) for path in paths]
-
-paths_sorted = np.array(paths)[np.argsort(shifts)[::1]]
-
-shifts = np.sort(shifts)
-
-entries = [None] * len(paths_sorted)
-
-for i in tqdm(range(len(entries))):
-    entry = {"shift": shifts[i], "results": []}
-    with open(paths_sorted[i]) as f:
-        next(f) # skip header
-        r = reader(f, delimiter=",")
-        for line in r:
-            e = np.array(line[1:len(line)], dtype=int)
-            if positive_only and e[len(e) - 1] == 0:
-                continue
-            if negative_only and e[len(e) - 1] == 1:
-                continue
-            entry["results"].append(e)
-    entry["results"] = np.array(entry["results"])
-    entries[i] = entry
-
-detectors = ["BlazeFace", "MTCNN", "RetinaFace", "SSD"]
-
-results = [None] * len(detectors)
-
-image_count = len(entries[0]["results"])
-
-fig, ax = plt.subplots(len(detectors) + 1, sharex=True)
-fig.suptitle(f"Accuracy when classifying {image_count} known{'-positive' if positive_only else '-negative' if negative_only else ''} images")
-fig.tight_layout()
-
-graph_xlabel = subtitle
-
-line_color = "blue" if graphing is None else "gray"
-
-y_min = float('inf')
-y_max = float('-inf')
-
-for i in range(len(detectors)):
-    results[i] = np.array([100 * np.sum(entry["results"][:, i] == entry["results"][:, len(detectors)]) / image_count for entry in entries])
-    if np.any(results[i] != 0.0):
-        y_min = np.min([y_min, np.min(results[i])])
-        y_max = np.max([y_max, np.max(results[i])])
-
-if y_min == y_max:
-    y_min -= 0.01
-    y_max += 0.01
-y_range = y_max - y_min
-y_padding = y_range / 10
-
-for i in tqdm(range(len(detectors))):
-    ax[i].set_ylim(y_min - y_padding, y_max + y_padding)
-    ax[i].set_title(detectors[i])
-    ax[i].set_ylabel("Accuracy (percent)")
-    ax[i].plot(shifts, results[i], color=line_color, linewidth=1)
-    if graphing is not None:
-        for j in range(0, np.max(shifts) + 1):
-            match graphing:
-                case "hue":
-                    color = hsv_to_rgb(j / 255, 1, 1)
-                case "saturation":
-                    color = hsv_to_rgb(0, j / 255, 0.9)
-                case "value":
-                    color = hsv_to_rgb(0, 1, j / 255)
-                case _:
-                    color = "red"
-            ax[i].plot(j, results[i][j], color=color, marker="o", markersize=3)
-    ax[len(detectors)].plot(shifts, results[i], linewidth=1)
-    
-ax[len(detectors)].set_ylim(y_min - y_padding, y_max + y_padding)
-ax[len(detectors)].set_title("All detectors")
-ax[len(detectors)].set_ylabel("Accuracy (percent)")
-ax[len(detectors)].set_xlabel(graph_xlabel)
-ax[len(detectors)].legend(detectors, loc="upper right", prop={"size": 8})
-
-if out_file is None:
-    plt.show()
-else:
-    plt.gcf().set_size_inches(1920/96, 1200/96)
-    plt.savefig(out_file)
+wrapper.graph_results(
+    results_dir="./results_temp",
+    detectors=[blazeface, insightface, mtcnn, ssd],
+    out_file="hue.png",
+    subtitle="Hue (0-255)",
+    graphing="hue", # must be one of "hue", "saturation", "value", or None
+    size=(1920, 1200),
+    dpi=96
+)
